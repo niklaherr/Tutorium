8270bf121af5c41b0889db9c60713761ab52d9d2
niklaherr <168107965+niklaherr@users.noreply.github.com>
2024-11-27 17:33:44 +0100

    Intial Commit

---
+++ src/Tutorium2/Aufgabe1.java
@@ -0,0 +1,7 @@
+package Tutorium2;
+
+public class Aufgabe1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Tutorium3/Aufgabe1.java
@@ -0,0 +1,7 @@
+package Tutorium3;
+
+public class Aufgabe1 {
+    public static void main(String[] args) {
+
+    }
+}
